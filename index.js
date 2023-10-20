7e35fe9436ea748a5e2debe330aed4978e9ec880
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 08:56:20 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "hill"
+
+function upperCaseCustomerName(){
+    if(typeof customerName === "string"){
+        customerName = customerName.toUpperCase()
+    };
+}
+
+
+function setBestCustomer(){
+     bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "her"}
